f38ca0e72dd5fad4db9d4b25f658e0b475484374
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2022-05-30 13:40:10 -0400

    commit tree 1c532ac268cf1717eea2d44b9e6b9530e13a7b05

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cards = [('Guadalupe','Ollie','Aki'),"surprise"];
+cards.push = [('Guadalupe')];
+function writeCards(cards) {
+for (let i=0; i < cards.length; i++) {
+console.log('Thank you,${cards[i]}, for the suprise gift!');
+debugger
+}
+return cards;
+}
+writeCards(cards);
+
+
+let countDown = (11);
+while (countDown <10) {
+console.log(countDown++);
+}
